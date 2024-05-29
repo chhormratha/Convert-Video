d735334707fc294f9a3d220d1f13fb5d3179d47e
chhormratha <163964985+chhormratha@users.noreply.github.com>
2024-05-29 21:21:56 +0700

    Update Convert Video.py

--- Convert Video.py
+++ Convert Video.py
@@ -12,8 +12,29 @@ def convert_videos(input_dir):
     os.makedirs(output_dir, exist_ok=True)
     os.makedirs(old_video_dir, exist_ok=True)
 
-    speed = '1.5'
-    flip = 'hflip,' # hflip,
+    vidoe_size = input('\n   Input Video Size (ex: 720:1280) : ')
+    if vidoe_size == '': 
+        vidoe_size = '720:1280'
+    else: vidoe_size = vidoe_size
+    speed = input('\n   Input Speed (default: 1) : ')
+    if speed == '':
+        speed = '1.0'
+    else: speed = speed
+    flip = input('\n   Flip Video (y/n) : ') # hflip,
+    if flip in ['y', 'yes']: 
+        flip = 'hflip,'
+    else: 
+        flip = ''
+    framrate = input('\n   Input Framrate (ex. 30) : ')
+    if framrate == '':
+        framrate = '30'
+    else: 
+        framrate =  framrate
+    bitrate = input('\n   Input Bitrate (ex. 3500) : ')
+    if bitrate == '':
+        bitrate = '3500k'
+    else:
+        bitrate = f'{bitrate}k'
 
     # Loop through all files in the input directory
     for file in os.listdir(input_dir):
@@ -25,9 +46,9 @@ def convert_videos(input_dir):
             # Construct FFmpeg command
             command = [
                 "ffmpeg", "-y", "-i", input_file,
-                "-vf", f"crop=iw-800:ih-100:400:50,scale=1920:1920,setsar=1:1, {flip} setpts=PTS/{speed},eq=saturation=1.5",
+                "-vf", f"crop=iw-20:ih-20:10:10,scale={vidoe_size},setsar=1:1, {flip} setpts=PTS/{speed},eq=saturation=1.5",
                 "-af", f"atempo={speed}",
-                "-r", "30", "-b:v", "3500k",
+                "-r", f"{framrate}", "-b:v", f"{bitrate}",
                 "-c:v", "h264_nvenc", "-c:a", "aac", "-strict", "experimental",
                 "-movflags", "+faststart", output_file
             ]
@@ -38,11 +59,8 @@ def convert_videos(input_dir):
                 
                 # Move the original video to the Old Video folder
                 os.rename(input_file, os.path.join(old_video_dir, file))
-            except subprocess.CalledProcessError as e:
-                print(f"Error processing {input_file}: {e}")
-                with open(os.path.join(output_dir, "error.log"), "a") as log_file:
-                    log_file.write(f"Error processing {input_file}: {e}\n")
+            except : pass
 
 if __name__ == "__main__":
-    input_dir = r"D:\Downloads\Video\Monkey"
+    input_dir = input('\n   Input Video Path : ').replace('"','')
     convert_videos(input_dir)
